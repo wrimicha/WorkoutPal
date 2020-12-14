0777dd783d377535bc03c0c2c33294fe97db2363
Michael Wright <wrimicha@sheridancollege.ca>
2020-12-13 19:14:52 -0500

    Added weight measurements, removed fab button

--- app/src/main/java/sheridan/wrimicha/final_project/MainActivity.kt
+++ app/src/main/java/sheridan/wrimicha/final_project/MainActivity.kt
@@ -21,13 +21,6 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-//    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)}
-//    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)}
-//    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)}
-//    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)}
-//
-//    private var clicked = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
@@ -43,39 +36,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
-//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-//            onFabButtonClicked()
-//        }
-
     }
-
-//    private fun onFabButtonClicked() {
-//        setVisibility(clicked)
-//        setAnimation(clicked)
-//        clicked = !clicked
-//    }
-
-//    private fun setAnimation(clicked: Boolean) {
-//        if(!clicked){
-//            findViewById<FloatingActionButton>(R.id.fab).startAnimation(rotateOpen)
-//            findViewById<FloatingActionButton>(R.id.addJog).startAnimation(fromBottom)
-//            findViewById<FloatingActionButton>(R.id.addBench).startAnimation(fromBottom)
-//        } else {
-//            findViewById<FloatingActionButton>(R.id.fab).startAnimation(rotateClose)
-//            findViewById<FloatingActionButton>(R.id.addJog).startAnimation(toBottom)
-//            findViewById<FloatingActionButton>(R.id.addBench).startAnimation(toBottom)
-//        }
-//    }
-//
-//    private fun setVisibility(clicked: Boolean) {
-//        if(!clicked){
-//            findViewById<FloatingActionButton>(R.id.addJog).visibility = View.VISIBLE
-//            findViewById<FloatingActionButton>(R.id.addBench).visibility = View.VISIBLE
-//        } else {
-//            findViewById<FloatingActionButton>(R.id.addJog).visibility = View.INVISIBLE
-//            findViewById<FloatingActionButton>(R.id.addBench).visibility = View.INVISIBLE
-//        }
-//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
